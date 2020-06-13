77abe751143c30817603563035dbf17221f81532
Mr.Chaos <Zhang-Jia@users.noreply.github.com>
2020-06-13 11:18:24 +0800

    set led

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
-forever(function () {
+input.buttonD12.onEvent(ButtonEvent.Click, function () {
     light.showAnimation(light.rainbowAnimation, 1000)
-    pins.i2cWriteNumber(
-    pins.i2cReadNumber(
-    5,
-    NumberFormat.Int8LE,
-    false
-    ),
-    0,
-    NumberFormat.Int8LE,
-    false
-    )
+})
+forever(function () {
+    music.playMelody("E D G F B A C5 B ", 120)
+    for (let index = 0; index < 4; index++) {
+        pins.i2cWriteNumber(
+        pins.i2cReadNumber(
+        5,
+        NumberFormat.Int8LE,
+        false
+        ),
+        0,
+        NumberFormat.Int8LE,
+        false
+        )
+    }
 })
